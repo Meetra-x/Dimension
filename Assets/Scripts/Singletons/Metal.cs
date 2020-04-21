5c404d3a704dc0dc1f4ee3c1000f921f0142fd54
Nate <mazu@live.co.uk>
2020-04-21 14:27:39 +0100

    Singleton & Button & Static string tag
    Included singleton pattern for resources, also included basic UI to test out code in scene which is incremental metal with button click and text to show

---
+++ Assets/Scripts/Singletons/Metal.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Metal : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static Metal instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+
+    public float MetalAmount { get; set; }
+
+    public float MetalIncrease { get; set; }
+    public float MetalDecrease { get; set; }
+
+    private Text MetalAmountText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MetalAmountText = GameObject.FindGameObjectWithTag(StringTags.MetalAmountTag).GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MetalAmountText.text = MetalAmount.ToString();
+    }
+
+    public void IncreaseMetalAmount(float amount)
+    {
+        MetalAmount += amount;
+    }
+
+}
